46f4dbfd2d5153f64680e3cf5a9b140a72a03ac9
manojkmj11 <mithuna692@gmail.com>
2023-04-10 14:18:23 +0530

    code changes

--- src/app/components/nurse/nurse-nav/updateinfo/updateinfo.component.ts
+++ src/app/components/nurse/nurse-nav/updateinfo/updateinfo.component.ts
@@ -44,12 +44,20 @@ export class UpdateinfoComponent implements OnInit {
     })
   }
 
+  allrgyForm !: FormGroup
+
   ngOnInit(): void {
 
     this.route.params.subscribe((data) => {
       this.patId = data['id']
       this.appId = data['AID']
       this.appointmentId = data['AID']
+    })
+
+    this.allrgyForm = this.fb.group({      
+        health_Id: this.patId,
+        appointment_Id: this.appId,
+        allergy: ['']
     })
 
     this.updateform = this.fb.group({
@@ -118,33 +126,34 @@ export class UpdateinfoComponent implements OnInit {
   }
 
   updateAllergy() {
-    let count : number = 0
-    if (this.updateform.valid) {
-      console.log(this.allergies.value?.at(0))
-      let len = Number(this.allergies.value?.length)
-      console.log(this.allergies.value)
-      for (let i = 0; i < len; i++) {
-        this.form1.allergy = this.allergies.value?.at(i)
-        console.log(this.allergies.value?.at(i))
-        console.log(this.form1)
-        this.Updatebasicrecord.savealergy(this.form1).subscribe((data) => {
-          console.log(data);
-          if(data){
-            count++
-          }
-        })
-      }
-        this.appoint.updateStatusByDoctor(this.appointmentId, 4).subscribe((data) => {
-          console.log(data)
-        })
-        this.openDialog('20ms','20ms');
+    console.log(this.allergies.value?.length);
+    // let count : number = 0
+    // if (this.updateform.valid) {
+    //   console.log(this.allergies.value?.at(0))
+    //   let len = Number(this.allergies.value?.length)
+    //   console.log(this.allergies.value)
+    //   for (let i = 0; i < len; i++) {
+    //     this.form1.allergy = this.allergies.value?.at(i)
+    //     console.log(this.allergies.value?.at(i))
+    //     console.log(this.form1)
+    //     this.Updatebasicrecord.savealergy(this.form1).subscribe((data) => {
+    //       console.log(data);
+    //       if(data){
+    //         count++
+    //       }
+    //     })
+    //   }
+    //     this.appoint.updateStatusByDoctor(this.appointmentId, 4).subscribe((data) => {
+    //       console.log(data)
+    //     })
+    //     this.openDialog('20ms','20ms');
 
-    }
-    else {
-      // show error message
-      console.log(this.updateform);
-      alert('Invalid form');
-    }
+    // }
+    // else {
+    //   // show error message
+    //   console.log(this.updateform);
+    //   alert('Invalid form');
+    // }
   }
 
   openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
